493e69a571a98ad8a8bd7eefb275568a70ecb25e
KCSAINITHIN <sainithin.cs23@bmsce.ac.in>
2025-08-07 10:33:28 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCFu2tKa7JszwPRE7dTrG9FEX3Giw-pavc",
+    authDomain: "grocera-nithin.firebaseapp.com",
+    projectId: "grocera-nithin",
+    storageBucket: "grocera-nithin.firebasestorage.app",
+    messagingSenderId: "824896008507",
+    appId: "1:824896008507:web:136d071d696f9ff190071e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
